e5bbf7c9f5ca6cb9dc0b5c80e510604feb09b60d
keefu <cyuplia@gmail.com>
2019-07-02 23:12:59 +0000

    Add Key

--- src/App.jsx
+++ src/App.jsx
@@ -6,48 +6,40 @@ import Header from "./Header.jsx";
 class App extends Component {
   constructor() {
     super()
-    this.state = {messages: 
-      [
+    this.state = {
+      currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
+      messages: [
         {
-          type: "incomingMessage",
-          content: "I won't be impressed with technology until I can download food.",
-          username: "Anonymous1"
+          username: "Bob",
+          content: "Has anyone seen my marbles?",
+          id:"ThIsIsAnID123",
         },
         {
-          type: "incomingNotification",
-          content: "Anonymous1 changed their name to nomnom",
-        },
-        {
-          type: "incomingMessage",
-          content: "I wouldn't want to download Kraft Dinner. I'd be scared of cheese packet loss.",
-          username: "Anonymous2"
-        },
-        {
-          type: "incomingMessage",
-          content: "...",
-          username: "nomnom"
-        },
-        {
-          type: "incomingMessage",
-          content: "I'd love to download a fried egg, but I'm afraid encryption would scramble it",
-          username: "Anonymous2"
-        },
-        {
-          type: "incomingMessage",
-          content: "This isn't funny. You're not funny",
-          username: "nomnom"
-        },
-        {
-          type: "incomingNotification",
-          content: "Anonymous2 changed their name to NotFunny",
-        },
-      ]}
+          username: "Anonymous",
+          content: "No, I think you lost them. You lost your marbles Bob. You lost them for good.",
+          id:"ThIsIsAnID321",
+        }
+      ]
+    }
+    
+  }
+  componentDidMount() {
+    console.log("componentDidMount <App />");
+    setTimeout(() => {
+      console.log("Simulating incoming message");
+      // Add a new message to the list of messages in the data store
+      const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
+      const messages = this.state.messages.concat(newMessage)
+      // Update the state of the app component.
+      // Calling setState will trigger a call to render() in App and all child components.
+      this.setState({messages: messages})
+    }, 3000);
   }
   render() {
     return (
       <div>
         <h1>Hello React :)</h1>
-        <ChatBar/>
+        <ChatBar currentUser={this.state.currentUser.name}/>
         <MessageList messages={this.state.messages}/>
         <Header/>
       </div>
--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -3,7 +3,7 @@ import Message from "./Message.jsx";
 
 class MessageList extends Component {
     render() {
-        let listOfMessages = this.props.messages.map(message => <Message username={message.username} content={message.content}/>)
+        let listOfMessages = this.props.messages.map(message => <Message username={message.username} content={message.content} key={message.id}/>)
         return(
             <main className="messages">
                 {listOfMessages}
